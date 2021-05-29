559e4aaff2a8f66169f5a33e641ed9549815e16d
Ivans Muzicenko <ivans.muzicenko@gmail.com>
2021-05-29 10:06:09 +0300

    First try of Vue.js

---
+++ app.js
@@ -0,0 +1,30 @@
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      inputValue: "",
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.inputValue);
+      this.inputValue = "";
+    },
+  },
+}).mount("#app");
+
+//----------vanilla JS version---------
+
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click', addGoal);
